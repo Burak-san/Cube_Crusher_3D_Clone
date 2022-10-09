5c874c1142a273c4a2c3f1c30e4ca01eb0f36fb5
ALİ ŞENESEN <ali.senesen@hotmail.com>
2022-10-09 18:56:03 +0300

    GameManagerAdded

---
+++ Assets/Scripts/Enums/GameStates.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enums
+{
+    public enum GameStates
+
+    {
+        PlayerTurn,
+        AtackTurn,
+        EnemyTurn,
+        MiniGameTurn
+    }
+}
---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Enums;
+using Signals;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameStates CurrentState;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    private void Start()
+    {
+        GameOpen();
+    }
+
+    #region  EventSubscription
+
+    private void OnEnable()
+    {
+        SubscribeEvents();
+    }
+
+    private void SubscribeEvents()
+    {
+        CoreGameSignals.Instance.onChangeGameState += OnChangeGameState;
+    }
+
+    
+
+    private void UnsubscribeEvents()
+    {
+        CoreGameSignals.Instance.onChangeGameState -= OnChangeGameState;
+    }
+
+    private void OnDisable()
+    {
+        UnsubscribeEvents();
+        GameClose();
+    }
+    
+    private void OnChangeGameState(GameStates newCurrentState)
+    {
+        CurrentState = newCurrentState;
+    }
+
+    private void GameOpen()
+    {
+        CurrentState = GameStates.PlayerTurn;
+        CoreGameSignals.Instance.onGameOpen?.Invoke();
+    }
+    private void GameClose()
+    {
+        CoreGameSignals.Instance.onGameClose?.Invoke();
+    }
+    
+
+    #endregion
+    
+}
+
+
+
