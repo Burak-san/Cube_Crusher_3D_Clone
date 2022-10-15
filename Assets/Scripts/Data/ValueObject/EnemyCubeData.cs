f161f2df729f0edc28b6e26599af1bc639fa12cc
ALİ ŞENESEN <ali.senesen@hotmail.com>
2022-10-15 11:50:49 +0300

    initial

---
+++ Assets/Scripts/Data/ValueObject/EnemyCubeData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Data.ValueObject
+{
+    [CreateAssetMenu(fileName = "CubeCrusher3D", menuName = "CubeCrusher3D/EnemyCubeData", order = 0)]
+    public class EnemyCubeData : ScriptableObject
+    {
+        public int EnemyHealthValue;
+    }
+}
