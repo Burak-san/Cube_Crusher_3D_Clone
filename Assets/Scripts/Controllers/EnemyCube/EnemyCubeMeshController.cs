fdce462cebfa944c2215c0debcabd7b9915ad533
Burak <60696929+Burak-san@users.noreply.github.com>
2022-10-19 23:14:59 +0300

    Enemy Cube Color Change commit

--- Assets/Scripts/Controllers/EnemyCube/EnemyCubeMeshController.cs
+++ Assets/Scripts/Controllers/EnemyCube/EnemyCubeMeshController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -7,7 +8,15 @@ namespace Controllers.EnemyCube
 {
     public class EnemyCubeMeshController : MonoBehaviour
     {
+        [SerializeField] private List<Material> materialList = new List<Material>();
+        private Renderer _renderer;
         private float _scale;
+
+        private void Awake()
+        {
+            _renderer = GetComponent<Renderer>();
+        }
+
         private void OnEnable()
         {
             GetRandomScale();
@@ -16,12 +25,36 @@ namespace Controllers.EnemyCube
 
         private void GetMaterial()
         {
-            
+
+            if (_scale <= 0.4f)
+            {
+                _renderer.material.color = materialList[0].color;
+            }
+            if (_scale>0.4f && _scale <= 0.6f)
+            {
+                _renderer.material.color = materialList[1].color;
+            }
+            if (_scale>0.6f && _scale <= 0.8f)
+            {
+                _renderer.material.color = materialList[2].color;
+            }
+            if (_scale>0.8f&& _scale <= 1.2f)
+            {
+                _renderer.material.color = materialList[3].color;
+            }
+            if (_scale>1.2f&& _scale <= 1.4f)
+            {
+                _renderer.material.color = materialList[4].color;
+            }
+            if (_scale>1.4f&& _scale <= 1.6f)
+            {
+                _renderer.material.color = materialList[5].color;
+            }
         }
         
         private void GetRandomScale()
         {
-            float tempScale = Random.Range(0.8f, 1.6f);
+            float tempScale = Random.Range(0.4f,1.6f);
             _scale = tempScale;
             transform.DOScaleY(tempScale, 3f).SetEase(Ease.OutElastic);
         }
