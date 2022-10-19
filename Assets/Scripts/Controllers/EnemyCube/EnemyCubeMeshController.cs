d5227087a18067bef1f6c7d77a1f05789a9525d4
Burak <60696929+Burak-san@users.noreply.github.com>
2022-10-19 17:00:28 +0300

    Enemy random scale system added. Cube mesh updated.

---
+++ Assets/Scripts/Controllers/EnemyCube/EnemyCubeMeshController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Controllers.EnemyCube
+{
+    public class EnemyCubeMeshController : MonoBehaviour
+    {
+        private float _scale;
+        private void OnEnable()
+        {
+            GetRandomScale();
+            GetMaterial();
+        }
+
+        private void GetMaterial()
+        {
+            
+        }
+        
+        private void GetRandomScale()
+        {
+            float tempScale = Random.Range(0.8f, 1.6f);
+            _scale = tempScale;
+            transform.DOScaleY(tempScale, 3f).SetEase(Ease.OutElastic);
+        }
+    }
+}
